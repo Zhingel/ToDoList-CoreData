248373bd8f8261b2667ca4c6abf0665643be32e6
Stas <staszhingel@icloud.com>
2021-01-16 17:03:51 +0300

    Initial Commit

---
+++ ToDoList+CoreData/ToDoList_CoreDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoList_CoreDataApp.swift
+//  ToDoList+CoreData
+//
+//  Created by Стас Жингель on 16.01.2021.
+//
+
+import SwiftUI
+
+@main
+struct ToDoList_CoreDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
